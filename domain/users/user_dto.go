eeaa086f0581247168c73814bd0d5291ea73444d
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-12 19:14:03 +0330

    add vildate method to user struct

---
+++ domain/users/user_dto.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"github.com/khalil-farashiani/microservice_users-api/utils/errors"
+	"strings"
+	"time"
+)
+
+type User struct {
+	Id          int64     `json:"id"`
+	FirstName   string    `json:"firstName"`
+	LastName    string    `json:"lastName"`
+	Email       string    `json:"email"`
+	DateCreated time.Time `json:"dateCreated"`
+}
+
+func (user *User) Validate() *errors.RestErr {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return errors.NewBadRequestError("invalid email address")
+	}
+	return nil
+}
--- services/users_service.go
+++ services/users_service.go
@@ -6,5 +6,8 @@ import (
 )
 
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
 	return &user, nil
 }
