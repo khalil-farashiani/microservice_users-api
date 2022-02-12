555b11a8cebec39e3a1a76482e28adf6206c33c4
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-12 21:39:53 +0330

    add Get function to get user from database

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,1 +1,25 @@
 package users
+
+import (
+	"fmt"
+	"github.com/khalil-farashiani/microservice_users-api/utils/errors"
+)
+
+var UserDB = make(map[int64]*User)
+
+func (user *User) Get() *errors.RestErr {
+	result := UserDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("user with %d id not found", user.Id))
+	}
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+	return nil
+}
+
+func (user User) Save() *errors.RestErr {
+	return nil
+}
