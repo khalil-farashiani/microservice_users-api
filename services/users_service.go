89d5d38aabbd3b78bf9f05f440cba4d0d516b461
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-13 01:24:10 +0330

    add GetUser function

--- services/users_service.go
+++ services/users_service.go
@@ -5,9 +5,21 @@ import (
 	"github.com/khalil-farashiani/microservice_users-api/utils/errors"
 )
 
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user, nil
 }
