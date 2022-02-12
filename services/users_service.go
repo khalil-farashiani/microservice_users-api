0255bde5b9c70302510769467d9dd79f905d2c38
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-12 18:29:31 +0330

    add CreateUser func to service

--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/khalil-farashiani/microservice_users-api/domain/users"
+	"github.com/khalil-farashiani/microservice_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
