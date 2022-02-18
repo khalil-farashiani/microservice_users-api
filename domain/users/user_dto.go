e64f65c04b573d94ce75d8b21ab197352f067f24
khalil-farashiani <khalilfarashi2000@gmail.com>
2022-02-18 14:44:51 +0330

    modify update function to use for update and partial update together in update function

--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -15,6 +15,8 @@ type User struct {
 
 func (user *User) Validate() *errors.RestErr {
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	user.FirstName = strings.TrimSpace(strings.ToLower(user.FirstName))
+	user.LastName = strings.TrimSpace(strings.ToLower(user.LastName))
 	if user.Email == "" {
 		return errors.NewBadRequestError("invalid email address")
 	}
--- services/users_service.go
+++ services/users_service.go
@@ -25,14 +25,30 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	return &user, nil
 }
 
-func UpdateUser(user users.User) (*users.User, *errors.RestErr) {
+func UpdateUser(ispartial bool, user users.User) (*users.User, *errors.RestErr) {
 	current, err := GetUser(user.Id)
 	if err != nil {
 		return nil, err
 	}
-	current.FirstName = user.FirstName
-	current.LastName = user.LastName
-	current.Email = user.Email
+
+	//if err := user.Validate(); err != nil {
+	//	return nil, err
+	//}
+	if ispartial {
+		if user.FirstName != "" {
+			current.FirstName = user.FirstName
+		}
+		if user.LastName != "" {
+			current.LastName = user.LastName
+		}
+		if user.Email != "" {
+			current.Email = user.Email
+		}
+	} else {
+		current.FirstName = user.FirstName
+		current.LastName = user.LastName
+		current.Email = user.Email
+	}
 
 	if err := current.Update(); err != nil {
 		return nil, err
